0d3a28b20ef6cc4480fecdf084d9f1db1a921166
deivyk296 <42418324+deivyk296@users.noreply.github.com>
2018-09-05 13:58:36 -0500

    Add files via upload

---
+++ avarange2.py
@@ -0,0 +1,15 @@
+print("HOLA")
+
+
+
+i=1
+sumaedades=0
+while i<=10:
+    print("CUAL ES TU EDAD", i,": ")    
+    edad=int(input())
+    i=i+1
+sumaedades = sumaedades + edad
+print("promedio es" ,sumaedades/10)
+    
+
+
