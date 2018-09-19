2d4a2e7e526a7fcda2ed8c04c88a59a83412ba9b
deivyk296 <42418324+deivyk296@users.noreply.github.com>
2018-09-19 15:00:37 -0500

    Add files via upload

---
+++ cimulador.1.py.py
@@ -0,0 +1,54 @@
+#simulador de credito
+#Daniel felipe obando lopez
+#Kevin david molina
+print(".:: SELECCIONA EL PRODUCTO DE TU INTERES ::.")
+print("***********************************************")
+print("1. Credito de libranza") 
+print("2. Credito de vivienda preaprobado")
+print("3. Credito compra de cartera")
+print("4. Credito compra de cartera de libranza")
+print("5. Credito de libre inversion")
+menu = input("Ingrese su opcion:")
+
+print(".:: INGRESO DE DATOS GENERALES ::. ")    
+valor = int(input("Monto del prestamo a solicitar ($): "))
+cuotas = int(input("Ingrese plazo en meses de las cuotas (1-60): "))
+ano_de_nacimiento=input("1918-2000 : 1999")
+while cuotas < 1 or cuotas > 60 :
+    if age >= 18 and age <=24 :
+        probabilidad = "Alta"
+    if age >= 25 and age <=55 :
+        probabilidad = "Media"
+else :
+    probabilidad = "Baja"
+while ano < 1918 or ano > 2000 :
+    ano = int(input("Error: ano de nacimiento no valido.ingrsese elanodenacimiento(1918-2000):"))
+
+if  TABLA_DE_AMORTIZACION
+    while i <= cuotas :
+        print("**",i)
+        print(",interes,")
+        print(",cuota_a_pagar")
+total_credito = round(total_credito + cuota_a_pagar,2)
+i = i + 1
+    print ("|Nro. cuota | Vr. cuota sin interes | Interes | Cuota a pagar |")
+    print ("********")
+    if opc == 1 :
+elif opc == 4:
+porc_interes = 1.7;
+else :
+porc_interes = 2;
+cuota_sin_int = round(valor / cuotas,2)
+interes = round((valor * porc_interes)/100,2)
+cuota_a_pagar = cuota_sin_int + interes
+print("|Nro. cuota | Vr. cuota sin interes | Interes | Cuota a pagar |")
+print("| Tasa de interes: ", porc_interes)
+print("| Total credito ($): ", total_credito)
+print("| Probabilidad de prestamo: ", probabilidad)
+
+
+
+    
+
+
+
